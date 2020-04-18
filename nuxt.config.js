3dc92c52fed3d15b9678b16493fce8c4ffc3dcf3
piterb <peter.bodi@yahoo.com>
2020-04-18 18:44:55 +0200

    Configuration to deploy heroku

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,5 @@
+import EventService from './services/EventService.js'
+
 export default {
   mode: 'universal',
   /*
@@ -19,7 +21,7 @@ export default {
   /*
    ** Customize the progress-bar color
    */
-  loading: { color: '#fff' },
+  loading: { color: '#39b982' },
   /*
    ** Global CSS
    */
@@ -57,5 +59,14 @@ export default {
      ** You can extend webpack config here
      */
     extend(config, ctx) {}
+  },
+  generate: {
+    routes: () => {
+      return EventService.getEvents().then((response) => {
+        return response.data.map((event) => {
+          return '/event/' + event.id
+        })
+      })
+    }
   }
 }
